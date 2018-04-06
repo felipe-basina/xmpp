329e1c052373db158f915d765871f434dfd30bd1
felipe.nascimento.ecommet@gmail.com <felipe.nascimento.ecommet@gmail.com>
2018-04-06 17:15:43 -0300

    Ajustes finos no codigo.

--- 1.hello-world/hello.js
+++ 1.hello-world/hello.js
@@ -49,28 +49,29 @@ $(document).ready(function () {
 });
 
 $(document).bind('connect', function (ev, data) {
-	var servidor = "http://localhost:5280/bosh/"
-	// "http://bosh.metajack.im:5280/xmpp-httpbind"
+	var servidor = "http://localhost:5280/bosh/";
 
 	var conn = new Strophe.Connection(servidor);
 	
 	conn.connect(data.jid, data.password, function (status) {
-	/*console.log('---> status: ' + status
-	+ ' : Strophe.Status.CONNECTED ' + Strophe.Status.CONNECTED
-	+ ' : Strophe.Status.DISCONNECTED ' + Strophe.Status.DISCONNECTED
-	+ ' : Strophe.Status.AUTHENTICATING ' + Strophe.Status.AUTHENTICATING
-	+ ' : Strophe.Status.DISCONNECTING ' + Strophe.Status.DISCONNECTING
-	+ ' : Strophe.Status.CONNECTING ' + Strophe.Status.CONNECTING
-	+ ' : Strophe.Status.CONNFAIL ' + Strophe.Status.CONNFAIL
-	+ ' : Strophe.Status.AUTHFAIL ' + Strophe.Status.AUTHFAIL);*/
-	if (status === Strophe.Status.CONNECTED) {
-		$(document).trigger('connected');
-	} else if (status === Strophe.Status.DISCONNECTED) {
-		$(document).trigger('disconnected');
-	} else if (status === Strophe.Status.CONNECTING) {
-		console.log('Tentando se conectar...');
-	}
-	Hello.connection = conn;
+		/*console.log('---> status: ' + status
+		+ ' : Strophe.Status.CONNECTED ' + Strophe.Status.CONNECTED
+		+ ' : Strophe.Status.DISCONNECTED ' + Strophe.Status.DISCONNECTED
+		+ ' : Strophe.Status.AUTHENTICATING ' + Strophe.Status.AUTHENTICATING
+		+ ' : Strophe.Status.DISCONNECTING ' + Strophe.Status.DISCONNECTING
+		+ ' : Strophe.Status.CONNECTING ' + Strophe.Status.CONNECTING
+		+ ' : Strophe.Status.CONNFAIL ' + Strophe.Status.CONNFAIL
+		+ ' : Strophe.Status.AUTHFAIL ' + Strophe.Status.AUTHFAIL);*/
+		if (status === Strophe.Status.CONNECTED) {
+			$(document).trigger('connected');
+		} else if (status === Strophe.Status.DISCONNECTED) {
+			$(document).trigger('disconnected');
+		} else if (status === Strophe.Status.CONNECTING) {
+			console.log('Tentando se conectar...');
+		}
+	});
+	
+	Hello.connection = conn;	
 });
 
 $(document).bind('connected', function () {
@@ -82,6 +83,7 @@ $(document).bind('connected', function () {
     var domain = Strophe.getDomainFromJid(Hello.connection.jid);
     
     Hello.send_ping(domain);
+
 });
 
 $(document).bind('disconnected', function () {
